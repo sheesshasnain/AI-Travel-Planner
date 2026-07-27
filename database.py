bee5db64c7af8b9613c7643ce3798e2a16a4b8c9
sheesshasnain <sms.sheess@gmail.com>
2026-07-27 15:36:44 +0500

    Migrate from SQLite to PostgreSQL

--- database.py
+++ database.py
@@ -1,17 +1,16 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "sqlite:///./travel_planner.db"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(
-    DATABASE_URL,
-    connect_args={"check_same_thread": False}
-)
+engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(
-    bind=engine,
     autocommit=False,
-    autoflush=False
+    autoflush=False,
+    bind=engine,
 )
 
 Base = declarative_base()
